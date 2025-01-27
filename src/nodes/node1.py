8be80c781fd0e3e2996eb3c4a5e69f4ca89534a2
bharatht19 <thippireddy.bharath@gmail.com>
2025-01-27 13:20:07 +0530

    added tools and system prompts
    Added placeholder tool bindings to llms and also the initial system prompts for both the agents nodes

--- src/nodes/node1.py
+++ src/nodes/node1.py
@@ -1,11 +1,20 @@
 from langchain_openai import ChatOpenAI
 from src.state.graph_state import GraphState
+from langchain_core.messages import SystemMessage
 
+research_tool=None
 def specialist_search(state: GraphState):
-    model = ChatOpenAI(model="gpt-4o")
-    query = state['query']
-    
-    specialist_result = model.invoke(f"specialist: {query}")
+    llm = ChatOpenAI(model="gpt-4o")
+    system_prompt = SystemMessage(
+        "Based on the provided doctor’s notes, perform a search to identify the "
+        "most suitable specialist and the corresponding template for generating a "
+        "recommendation letter")
+
+    tools = [research_tool]
+
+    llm_with_tools = llm.bind_tools(tools)
+
+    specialist_result = llm_with_tools.invoke([system_prompt]+state['messages'])
     
     return {
         "research_results": [specialist_result.content],
--- src/nodes/node2.py
+++ src/nodes/node2.py
@@ -1,11 +1,23 @@
 from langchain_openai import ChatOpenAI
 from src.state.graph_state import GraphState
+from langchain_core.messages import SystemMessage
 
+
+analysis_tool = None
 def analyze_node(state: GraphState):
-    model = ChatOpenAI(model="gpt-4o")
-    research_results = state['research_results']
-    
-    analysis = model.invoke(f"Analyze these research results: {research_results}")
+    llm = ChatOpenAI(model="gpt-4o")
+
+    system_prompt = SystemMessage(
+        "Based on the provided recommendation letter template, perform a search to retrieve the relevant patient data."
+        " Use the retrieved patient data, along with the provided recommendation letter template and doctor information, "
+        "to generate a complete recommendation letter for the specialist. Additionally, draft a professional email that "
+        "can be sent to the patient, informing them about the recommendation and providing relevant details.")
+
+    tools = [analysis_tool]
+
+    llm_with_tools = llm.bind_tools(tools)
+
+    analysis = llm_with_tools.invoke([system_prompt]+state['research_results'])
     
     return {
         "analysis": analysis.content,
