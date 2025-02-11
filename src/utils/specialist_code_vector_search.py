fa10da75187de9ad7e86f7931030605c3249c811
mick.lynch@gmail.com <Mick Lynch>
2025-02-11 17:12:39 -0500

    Find appropriate specialist codes

---
+++ src/utils/specialist_code_vector_search.py
@@ -0,0 +1,23 @@
+from astrapy import DataAPIClient
+from typing import List
+from console_config import pretty_print_json, print_ruler, print_section_header
+import os
+
+# Initialize the client
+client = DataAPIClient(os.getenv("ASTRA_DB_API_KEY"))
+db = client.get_database_by_api_endpoint(os.getenv("ASTRA_DB_URL"))
+collection = db.get_collection("specialists_taxonomy")
+
+print_ruler()
+print_section_header(f"Connected to Astra DB: {db.list_collection_names()}", "green")
+
+def find_specialist(reason: str, result_size: int=3) -> List[str]: 
+    specialists_type = list(collection.find(
+    sort={"$vectorize": reason},
+    limit=result_size,
+    ))
+    pretty_print_json(specialists_type)
+    codes = codes = [item['Code'] for item in specialists_type]
+    return codes
+
+find_specialist("broken hip that needs replacement", 3)
